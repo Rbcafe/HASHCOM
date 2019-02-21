24564de5d22d48599b055f085bbd74f464ceaa67
Rbcafe <Webmaster@rbcafe.com>
2019-02-21 11:52:51 +0100

    Update hashcomnf.sh

--- hashcomnf.sh
+++ hashcomnf.sh
@@ -1,1 +1,2 @@
-echo "";echo "Hashcom";echo "";n=$(cat $1|shasum -a 256|cut -c1-8);echo "/* $n */";echo "";
+echo "";echo "Hashcom";echo "";if test -a "$1"; then n=$(cat $1|shasum -a 256|cut -c1-8);echo "/* $n */";echo "";else echo "[Info] no file or bad file...";echo "";fi
+
