30dcf6db4d8d89a6ef56df8bf056ea7c82fe8915
Rbcafe <Webmaster@rbcafe.com>
2019-02-21 11:51:50 +0100

    Update hashcom.sh

--- hashcom.sh
+++ hashcom.sh
@@ -1,1 +1,1 @@
-figlet hashcom;n=$(cat $1|shasum -a 256|cut -c1-8);echo "/* $n */";echo "";
+figlet hashcom;if test -a "$1"; then n=$(cat $1|shasum -a 256|cut -c1-8);echo "/* $n */";echo "";else echo "[Info] no file or bad file...";echo "";fi
